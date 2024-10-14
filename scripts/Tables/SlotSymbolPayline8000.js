02f17e52f2469474396ad92ace265931bf250036
duno <hue.nguyen@enotion.io>
2024-10-14 10:29:22 +0700

    update

--- scripts/Tables/SlotSymbolPayline8000.js
+++ scripts/Tables/SlotSymbolPayline8000.js
@@ -1,3 +1,10 @@
+const { convertAssetArrayToObject } = require('slotUtils');
 cc.Class({
     extends: require('SlotSymbolPaylinev2'),
+
+    onLoad() {
+        this._super();
+        this.assets = convertAssetArrayToObject(this.symbols);
+    },
+
 });
